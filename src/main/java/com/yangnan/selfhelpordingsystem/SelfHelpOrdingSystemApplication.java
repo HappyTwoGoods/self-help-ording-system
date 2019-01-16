3dca663393b4acc161837feb057c7e23a9a11b80
yanrui <1131411057@qq.com>
2019-01-16 16:11:49 +0800

    init system

---
+++ src/main/java/com/yangnan/selfhelpordingsystem/SelfHelpOrdingSystemApplication.java
@@ -0,0 +1,14 @@
+package com.yangnan.selfhelpordingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfHelpOrdingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfHelpOrdingSystemApplication.class, args);
+	}
+
+}
+
