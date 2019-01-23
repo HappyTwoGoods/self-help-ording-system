36942978858550484921a4087e52492c18d69e1b
yanrui <1131411057@qq.com>
2019-01-23 16:08:02 +0800

    addConstant

---
+++ src/main/java/com/yangnan/selfhelpordingsystem/constant/GoodsType.java
@@ -0,0 +1,35 @@
+package com.yangnan.selfhelpordingsystem.constant;
+
+public class GoodsType {
+
+    /**
+     * 主食
+     */
+    private static final int STAPLE_FOOD = 1;
+
+    /**
+     * 饮品
+     */
+    private static final int DRINKS = 2;
+
+    /**
+     * 汤羹
+     */
+    private static final int SOUPS = 3;
+
+    /**
+     * 炒菜
+     */
+    private static final int SAUTE = 4;
+
+    /**
+     * 凉菜
+     */
+    private static final int COLD_DISHES = 5;
+
+    /**
+     * 烧烤
+     */
+    private static final int BARBECUE = 6;
+
+}
